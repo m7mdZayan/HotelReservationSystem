c58055f2a59b16e6fac05a9d9ee6debaf7133e48
unknown <m7mdzayan@gmail.com>
2021-03-22 17:44:26 +0200

    add some views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('admin/dashboard');
 });
+
+Route::get('/manager', function () {
+    return view('manager/dashboard');
+});
+
+Route::get('/user', function () {
+    return view('user/dashboard');
+});
+
+Route::get('/receptionist', function () {
+    return view('receptionist/dashboard');
+});
+
