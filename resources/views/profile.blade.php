3651451b0a8deac53c90d94e2850d0030ce89ce0
NadineAbdelfattah <78588221+NadineAbdelfattah@users.noreply.github.com>
2021-03-24 17:37:26 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <img src="/uploads/avatars/{{ $user->avatar_image }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
             <h2>{{ $user->name }}'s Profile</h2>
             <form enctype="multipart/form-data" action="/profile" method="POST">
                 <label>Update Profile Image</label>
