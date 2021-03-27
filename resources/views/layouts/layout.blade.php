8f5f12124d58d0e1f3dda8680a6f7c3bee4c69d6
unknown <m7mdzayan@gmail.com>
2021-03-27 10:44:01 +0200

    layout file modified

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -131,14 +131,14 @@
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('manager.floors') }}" class="nav-link">
+                <a href="{{ route('floors.index') }}" class="nav-link">
                     <i class="fas fa-building mr-2"></i>
                     <p>Manage Floors</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('manager.rooms') }}" class="nav-link">
+                <a href="{{ route('rooms.index') }}" class="nav-link">
                     <i class="fas fa-th mr-2"></i>
                     <p>Manage Rooms</p>
                 </a>
@@ -174,14 +174,14 @@
 
 
             <li class="nav-item">
-                <a href="{{ route('manager.floors') }}" class="nav-link">
+                <a href="{{ route('floors.index') }}" class="nav-link">
                     <i class="fas fa-building mr-2"></i>
                     <p>Manage Floors</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('manager.rooms') }}" class="nav-link">
+                <a href="{{ route('rooms.index') }}" class="nav-link">
                     <i class="fas fa-th mr-2"></i>
                     <p>Manage Rooms</p>
                 </a>
