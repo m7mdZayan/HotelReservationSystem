c2fb44d73d03eb645aa71d48605bc5216071f43b
EsraaFarhat <esraafarhat97@gmail.com>
2021-03-27 13:03:45 +0200

    Edit

--- resources/views/floors/actions.blade.php
+++ resources/views/floors/actions.blade.php
@@ -1,6 +1,6 @@
 <a class="btn btn-primary" href="{{route('floors.edit', ['id'=>$id] )}}" >Edit</a>
 
-<form method="post" action="{{ route('floors.destroy', ['id' => $id]) }}">
+<form method="post" style="display: inline" action="{{ route('floors.destroy', ['id' => $id]) }}">
                   @csrf
                   {{ method_field("DELETE") }}
                   <input type="submit" onclick="return confirm ('are you sure?')" class="btn btn-danger" value="Delete"> 
